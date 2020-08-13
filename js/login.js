edcbe846b613bed8cbcb9f13c0a85d7ff195ce31
nl96 <68866318+nl96@users.noreply.github.com>
2020-08-13 18:11:02 +0300

    Login added

--- js/login.js
+++ js/login.js
@@ -3,4 +3,28 @@
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
 
+    document.getElementById("submit").addEventListener("click", function(e){
+        let inputEmail = document.getElementById("userName");
+        let inputPassword = document.getElementById("userPassword");
+        let inputsOccupied = true;
+    
+        if (inputEmail.value === "" || inputPassword.value === ""){
+            inputsOccupied = false;
+        }
+    
+        let invalid = document.getElementById("invalid");
+
+        if (inputsOccupied){
+            invalid.innerHTML = "";
+            window.location = "home.html";
+        } else {
+            invalid.innerHTML = `
+                <div class="alert alert-danger" role="alert" style="position: relative; width:auto; top: 0;">
+                    <p class="mb-0">Usuario y contraseña que ingresaste no son válidos. Por favor, revisa e inténtalo de nuevo.</p>
+                </div>
+            `;
+        }
+
+    });
+
 });
