bd844044da465b4143c09019f97e5ffd961d77a2
nl96 <68866318+nl96@users.noreply.github.com>
2020-08-19 22:16:18 +0300

    Filter price added

--- js/products.js
+++ js/products.js
@@ -1,27 +1,37 @@
 var productsArray = [];
 
+var minCost = undefined;
+var maxCost = undefined;
+
 function showProductsList(array){
 
     let htmlContentToAppend = "";
     for(let i = 0; i < array.length; i++){
         let products = array[i];
 
-        htmlContentToAppend += `
-        <a href="product-info.html" class="list-group-item list-group-item-action">
-            <div class="row">
-                <div class="col-3">
-                    <img src="` + products.imgSrc + `" alt="` + products.description + `" class="img-thumbnail">
-                </div>
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                        <h4 class="mb-1">`+ products.name +`</h4>
-                        <small class="text-muted">` + products.soldCount + ` artículos</small>
+        if ((minCost == undefined || (minCost != undefined && minCost <= parseInt(products.cost))) && 
+            (maxCost == undefined || (maxCost != undefined && maxCost >= parseInt(products.cost)))) {
+
+            htmlContentToAppend += `
+            <a href="product-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + products.imgSrc + `" alt="` + products.description + `" class="img-thumbnail">
                     </div>
-                    <p class="mb-1">` + products.description + `</p>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ products.name +`</h4>
+                            <small class="text-muted">` + products.soldCount + ` artículos</small>
+                        </div>
+                        <p class="mb-1">` + products.description + `</p>
+                        <div class="d-flex w-100">
+                            <span class="h4 mb-1 mt-2 font-weight-bolder">`+ products.currency +" "+ products.cost + `</span>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </a>
-        `
+            </a>
+            `
+        }
 
         document.getElementById("prod-list-container").innerHTML = htmlContentToAppend;
     }
@@ -38,5 +48,39 @@ document.addEventListener("DOMContentLoaded", function (e) {
             //Muestro los productos ordenados
             showProductsList(productsArray);
         }
+    });
+    
+
+    document.getElementById("rangeFilterCost").addEventListener("click", function(){
+        //Obtengo el mínimo y máximo de los intervalos para filtrar por cantidad
+        //de productos por categoría.
+        minCost = document.getElementById("rangeFilterCostMin").value;
+        maxCost = document.getElementById("rangeFilterCostMax").value;
+
+        if ((minCost != undefined) && (minCost != "") && (parseInt(minCost)) >= 0){
+            minCost = parseInt(minCost);
+        }
+        else{
+            minCost = undefined;
+        }
+
+        if ((maxCost != undefined) && (maxCost != "") && (parseInt(maxCost)) >= 0){
+            maxCost = parseInt(maxCost);
+        }
+        else{
+            maxCost = undefined;
+        }
+
+        showProductsList(productsArray);
+    });
+
+    document.getElementById("clearRangeFilter").addEventListener("click", function(){
+        document.getElementById("rangeFilterCostMin").value = "";
+        document.getElementById("rangeFilterCostMax").value = "";
+
+        minCost = undefined;
+        maxCost = undefined;
+
+        showProductsList(productsArray);
     });
 });
